e2d8bfc79744a622418bd83b93a60de42a5449cc
Sachin Dhakal <21btrcs253@jainuniversity.ac.in>
2025-04-13 01:20:00 +0530

    update

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ document.querySelectorAll(".projects-box").forEach(box => {
     });
   });
 
+  const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
   
   
 
